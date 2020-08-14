8616df290064cf1faf846a156b67d558e3adc533
jonathan karta <Jonathan.karta@s.afeka.ac.il>
2020-08-14 13:32:45 +0300

    added doors

---
+++ Assets/Scripts/DoorMotion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMotion : MonoBehaviour
+{
+    public Animator animator;
+    public bool isOpen;
+    public GameObject doorAxis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = doorAxis.GetComponent<Animator>();
+        isOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            animator.SetTrigger("Open");
+            isOpen = true;
+        }   
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (isOpen)
+        {
+            animator.SetTrigger("Close");
+            isOpen = false; 
+        }        
+    }
+}
